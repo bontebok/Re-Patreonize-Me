deb26cd32a73d8914683f10f696bbd07c54b485e
Rucio <github@ungulate.net>
2024-09-21 05:11:24 -0400

    Added badge order fix and provided new setting to enable or disable

--- RePatreonizeMe/RePatreonizeMe.cs
+++ RePatreonizeMe/RePatreonizeMe.cs
@@ -12,7 +12,7 @@ namespace RePatreonizeMe
     {
         public override string Name => "Re-Patreonize Me";
         public override string Author => "Rucio";
-        public override string Version => "0.0.4";
+        public override string Version => "0.1.0";
         public override string Link => "https://github.com/bontebok/Re-Patreonize-Me";
 
         public enum SupporterBadge
@@ -29,22 +29,45 @@ namespace RePatreonizeMe
             { SupporterBadge.ResoniteSupporter, new("resdb:///ac586a5995e02cd4fd49091afd7d437715a53749127b1145fbded5ccdc850f2a") }
         };
 
+        private static readonly Dictionary<string, long> BadgeOrderOffsets = new()
+        {
+            { "Mobile", -80},
+            { "Linux", -70},
+            { "Host", -60},
+            { "Team", -50},
+            { "Moderator", -40},
+            { "Mentor", -30},
+            { "Translator", -20},
+            { "Supporter", -10},
+        };
+
+        private static long GetBadgeOrder(string badge)
+        {
+            if (BadgeOrderOffsets.TryGetValue(badge, out long offset))
+                return offset;
+            return 0;
+        }
+
         private static Uri GetSupporterBadge(SupporterBadge badge)
         {
             return SupporterBadgeUri[badge];
         }
 
         [AutoRegisterConfigKey]
-        public static readonly ModConfigurationKey<SupporterBadge> BADGE_CHOICE = new("supporterBadge", "Supporter Badge: Select which supporter badge you'd like to use.", () => SupporterBadge.OldPatreonLogo);
+        public static readonly ModConfigurationKey<bool> CHANGE_BADGE = new("changeBadge", "Change supporter badge? Leave unchecked to use the default.", () => true);
 
         [AutoRegisterConfigKey]
-        public static readonly ModConfigurationKey<colorX> LOGO_COLOR = new("logoColor", "Tint Color: Customize the tint color of the badge.", () => new colorX(1f, 0.25882352941f, 0.30196078431f));
+        public static readonly ModConfigurationKey<bool> FIX_ORDER = new("fixBadgeOrder", "Fix badge order? Adds order offsets to correct badge order.", () => true);
 
         [AutoRegisterConfigKey]
-        public static readonly ModConfigurationKey<Uri> CUSTOM_IMAGE_URI = new("customImageUri", "Custom Image: resdb/http/https uri to a custom image. (128x128 resolution).", () => null);
+        public static readonly ModConfigurationKey<SupporterBadge> BADGE_CHOICE = new("supporterBadge", "Select which supporter badge you'd like to use.", () => SupporterBadge.OldPatreonLogo);
 
         [AutoRegisterConfigKey]
-        public static readonly ModConfigurationKey<bool> DISABLEMOD = new("disableMod", "Disable Mod: Do not change supporter badge.", () => false);
+        public static readonly ModConfigurationKey<colorX> LOGO_COLOR = new("logoColor", "Customize the tint color of the badge. Set to full white to prevent tinting.", () => new colorX(1f, 0.25882352941f, 0.30196078431f));
+
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<Uri> CUSTOM_IMAGE_URI = new("customImageUri", "To use a custom badge icon, provide a resdb/http/https uri to a custom image. (128x128 resolution).", () => null);
+
 
         public static ModConfiguration Config;
 
@@ -115,7 +138,6 @@ namespace RePatreonizeMe
                         OnSupporterMeshAdded(child, SupporterBadge);
 
                     AddPatreonBadge(child);
-                    child.Name = "Patreon Supporter";
                 }
             }
 
@@ -132,29 +154,77 @@ namespace RePatreonizeMe
                 OnSupporterBadgeAdded(null, SupporterSlot);
             }
 
+            private static void FixBadgeOrder(Slot BadgeSlot)
+            {
+                foreach (var child in BadgeSlot.Children)
+                {
+                    child.OrderOffset = GetBadgeOrder(child.Name);
+                }
+            }
+
+            private static void UpdateBadges(Slot Badges, float BadgeSize, float BadgeSeparation, int MaxRowSize)
+            {
+                int rowSize = Math.Max(MaxRowSize, 1);
+                int i = 0;
+
+                foreach (Slot child in Badges.Children)
+                {
+                    int x = i % rowSize;
+                    int y = i / rowSize;
+                    var float3 = new float3(x, y);
+                    child.LocalPosition = float3 * (BadgeSize + BadgeSeparation);
+                    i++;
+                }
+            }
+
+            [HarmonyPatch(typeof(AvatarManager))]
+            public class AvatarManagerPatch
+            {
+                [HarmonyPostfix]
+                [HarmonyPatch("OnAwake")]
+                public static void AvatarManagerOnAwakePostfix(AvatarManager __instance, SyncRef<Slot> ____badgeTemplates)
+                {
+                    if (!__instance.IsUnderLocalUser) // Should only apply to local user
+                        return;
+
+                    __instance.Slot.RunInUpdates(30, () =>
+                    {
+                        var BadgesRoot = ____badgeTemplates.Target;
+                        if (Config.GetValue(FIX_ORDER))
+                            FixBadgeOrder(BadgesRoot);
+
+                        if (Config.GetValue(CHANGE_BADGE))
+                            OnBadgesAdded(__instance.Slot, BadgesRoot);
+                    });
+                }
+            }
+
             [HarmonyPatch(typeof(AvatarBadgeManager))]
             public class AvatarBadgeManagerPatch
             {
                 [HarmonyPostfix]
                 [HarmonyPatch("OnAwake")]
-                public static void AvatarBadgeManagerOnAwakePostfix(AvatarBadgeManager __instance, SyncRef<Slot> ____badgesRoot)
+                public static void AvatarBadgeManagerOnAwakePostfix(AvatarBadgeManager __instance, SyncRef<Slot> ____badgesRoot,
+                    Sync<float> ___BadgeSize, Sync<float> ___BadgeSeparation, Sync<int> ___MaxRowSize)
                 {
+                    if (!__instance.IsUnderLocalUser) // Should only apply to local user
+                        return;
+
                     __instance.Slot.RunInUpdates(60, () =>
                     {
-                        if (Config.GetValue(DISABLEMOD))
-                            return;
-
-                        if (!__instance.IsUnderLocalUser) // Should only apply to local user
-                            return;
-
                         var BadgesRoot = ____badgesRoot.Target;
 
                         if (BadgesRoot == null)
-                        {
                             return;
+
+                        if (Config.GetValue(FIX_ORDER))
+                        {
+                            FixBadgeOrder(BadgesRoot);
+                            UpdateBadges(BadgesRoot, ___BadgeSize.Value, ___BadgeSeparation.Value, ___MaxRowSize.Value);
                         }
 
-                        OnBadgesAdded(__instance.Slot, BadgesRoot);
+                        if (Config.GetValue(CHANGE_BADGE))
+                            OnBadgesAdded(__instance.Slot, BadgesRoot);
                     });
                 }
             }
